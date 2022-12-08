5465487f3e50b9b1c38234e03f3d0b74d5ebc0d8
Diego Wanderley <dswanderley@gmail.com>
2022-12-08 15:18:44 +0100

    Update examples.cpp
    Add pointers to constant data use cases.

--- const/examples.cpp
+++ const/examples.cpp
@@ -24,5 +24,37 @@ int main() {
     std::cout << "Const y = " << y << std::endl;
 
 
+    /**
+     *  POINTERS
+     */
+
+    /* POINTER TO CONSTANT DATA */
+    const int *p_cint;          // *p_cint is a "const int" - usually refer as "const pointer"
+
+    // Case 1 - point to const
+    p_cint = &x;
+    std::cout << "p_cint (" << &p_cint << ")" << std::endl;
+    std::cout << "Pointer to: " << p_cint << ", value: " << *p_cint << std::endl;
+
+    // Case 2 - change to another const
+    p_cint = &y;
+    std::cout << "p_cint (" << &p_cint << ")" << std::endl;
+    std::cout << "Pointer to: " << p_cint << ", value: " << *p_cint << std::endl;
+
+    // Case 3 - change const value
+    // *p_cint = 10;    /* error: assignment of read-only location '* p_cint' */
+
+    // Case 4 - point to a variable
+    int a = 12;
+
+    p_cint = &a;
+    std::cout << "p_cint (" << &p_cint << ")" << std::endl;
+    std::cout << "Pointer to: " << p_cint << ", value: " << *p_cint << std::endl;
+
+    // Case 5 - Change variable value
+    // *p_cint = 10;    /* error: assignment of read-only location */
+
+
+
     return 0;
 }
