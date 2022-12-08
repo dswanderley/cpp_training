3e503608c9002a5f0e14a79be7e39a9be9507892
Diego Wanderley <dswanderley@gmail.com>
2022-12-08 16:13:04 +0100

    Update sintaxe.cpp
    Add error cases.

--- const/sintaxe.cpp
+++ const/sintaxe.cpp
@@ -23,6 +23,9 @@ int main() {
     const int c2 = 2;
     std::cout << "Const y = " << c2 << std::endl;
 
+    // c1 = 10;        /* error: assignment of read-only variable 'c1' */
+
+    // c1 = c2;        /* error: assignment of read-only variable 'c1' */
 
     return 0;
 }
