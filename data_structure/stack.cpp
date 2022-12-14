ade6ded10b76e1cb22ca85b2f1e14a849650e553
Diego Wanderley <dswanderley@gmail.com>
2022-12-14 18:32:16 +0100

    Update stack.cpp
    Add print method.

--- data_structure/stack.cpp
+++ data_structure/stack.cpp
@@ -18,6 +18,7 @@ public:
     bool isFull() { return (height == max); };
     unsigned int getSize() { return height; };
 
+    void print();
     bool push(const T& item);
     T    pop();
     T    peek();
@@ -61,35 +62,58 @@ T Stack<T, max>::pop()
 }
 
 
+template <typename T, unsigned int max>
+void Stack<T, max>::print()
+{
+    if (isEmpty()){
+        std::cout << "STACK EMPTY";
+    }
+    else {
+        std::cout << "STACK: | ";
+        for (int i = 0; i < height; i++) {
+        std::cout << i << " | ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+
 int main() {
 
     Stack<int, 5> stack05;
     Stack<int> stack10;
 
-
+    // Push data
     for (int i = 0; i < 10; i++) {
 
-        if (!stack05.isFull())
+        if (!stack05.isFull()) {
             stack05.push(i+1);
+            stack05.print();
+        }
 
         if (!stack10.isFull())
             stack10.push(i+1);
     }
 
+    stack10.print();
 
+    // Pop data
     while (stack10.getSize() > 0) {
 
         if (!stack05.isEmpty()) {
             auto item05 = stack05.pop();
+            std::cout << "Value " << item05 << " popped from stack05." << std::endl;
         }
 
         if (!stack10.isEmpty()) {
             auto item10 = stack10.pop();
+            std::cout << "Value " << item10 << " popped from stack10." << std::endl;
         }
 
     }
 
-
+    stack05.print();
+    stack10.print();
 
     return 0;
 }
