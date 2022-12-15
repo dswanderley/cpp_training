695d50a42ff8a2c3425dcd467824a3fa82aeee40
Diego Wanderley <dswanderley@gmail.com>
2022-12-15 14:52:07 +0100

    Update queue.cpp
    Add pop.

--- data_structure/queue.cpp
+++ data_structure/queue.cpp
@@ -14,9 +14,12 @@ class Queue
 
     bool isEmpty() const { return (length == 0); }
     bool isFull() const { return (length == size); }
+    uint getLength() { return length; }
     T lookFirst() const { return queue[0]; }
 
     T& operator[](uint index) const;
+    T pop();
+    void push(const T& item);
     void print() const;
 
  private:
@@ -50,6 +53,35 @@ T& Queue<T, size>::operator[](uint index) const
     }
 
     return queue[index];
+}
+
+template<class T, uint size>
+void Queue<T, size>::push(const T& item)
+{
+
+}
+
+template<class T, uint size>
+T Queue<T, size>::pop()
+{
+    T item{};
+
+    if (length == 0)
+    {
+        std::cout << "EMPTY QUEUE!" << std::endl;
+    }
+    else
+    {
+        item = std::move( queue[0] );
+
+        for (uint i = 0; i < length; i++)
+        {
+            queue[i] = std::move( queue[i+1] );
+        }
+        length--;
+    }
+
+    return item;
 }
 
 
