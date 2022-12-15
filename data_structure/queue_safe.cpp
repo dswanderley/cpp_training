9438fa6b94270c4d66adf93b1698ae8cb9474dd9
Diego Wanderley <dswanderley@gmail.com>
2022-12-15 18:15:19 +0100

    Update queue_safe.cpp
    Add test case and conditions to lock function.

--- data_structure/queue_safe.cpp
+++ data_structure/queue_safe.cpp
@@ -5,6 +5,9 @@
 #include <condition_variable>
 
 
+using namespace std::chrono_literals;
+
+
 typedef unsigned int uint;
 
 
@@ -65,16 +68,32 @@ T Queue<T, size>::pop()
         length--;
     }
 
-    return std::move(item);
+    return item;
 }
 
 template<class T, uint size>
 T Queue<T, size>::pop_safe()
 {
-    
 
+    {
+        // unique_lock prevents mutex from getting locked in case of exception
+        std::unique_lock<std::mutex> lock(mutex);
+        // The difference between is that you can lock and unlock a std::unique_lock.
+        // std::lock_guard will be locked only once on construction and unlocked on destruction.
+        // std::unique_lock can also be passed to a function (see below).
+        condition.wait(lock, [this]() -> bool
+        {
+            return (length != 0);
+        });
+    }
+
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        const T item = pop();
+
+        return item;
+    }
 }
-
 
 template<class T, uint size>
 bool Queue<T, size>::push(const T& item)
@@ -97,9 +116,11 @@ template<class T, uint size>
 bool Queue<T, size>::push_safe(const T& item)
 {
     bool pushed = false;
-    // unique_lock prevents mutex from getting locked in case of exception
-    std::unique_lock<std::mutex> lock(mutex);
-    pushed = push(item);
+    {
+        // unique_lock prevents mutex from getting locked in case of exception
+        std::unique_lock<std::mutex> lock(mutex);
+        pushed = push(item);
+    }
     // Alert all that mutex was released
     condition.notify_all();
 
@@ -124,23 +145,40 @@ void Queue<T, size>::print() const
 
 int main()
 {
-    Queue<int, 5> queue;
+    Queue<int, 20> queue;
 
-    int i = 1;
+    std::thread client1 ([&] {
+        for (uint i = 0; i < 10; i++){
+            if (!queue.isEmpty()) {
+                auto item = queue.pop_safe();
+                std::cout << "Client1 -> "<< item << std::endl;
+            }
+            std::this_thread::sleep_for(200ms);
+        }
+    });
 
-    while (!queue.isFull()) {
-        if (queue.push(i++))
-            queue.print();
-    }
-    //queue.push(i++);
+    std::thread client2 ([&] {
+        for (uint i = 0; i < 12; i++){
+            std::this_thread::sleep_for(300ms);
 
-    while (!queue.isEmpty()) {
-        bool empty = queue.isEmpty();
-        auto item = queue.pop();
-        std::cout << "Value " << item << " popped." << std::endl;
-        queue.print();
-    }
-    //auto item = queue.pop();
+            auto item = queue.pop_safe();
+            std::cout << "Client2 -> "<< item << std::endl;
+
+        }
+    });
+
+    std::thread source ([&] {
+
+        for (uint i = 0; i < 20; i++){
+            queue.push_safe(i);
+        }
+        std::this_thread::sleep_for(500ms);
+
+    });
+
+    source.join();
+    client1.join();
+    client2.join();
 
     return 0;
 }
