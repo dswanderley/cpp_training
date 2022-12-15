b7e3950ea93ed3ffe7584afcf73f358f59616ea0
Diego Wanderley <dswanderley@gmail.com>
2022-12-15 12:54:15 +0100

    Update queue.cpp
    Add auxiliar functions.

--- data_structure/queue.cpp
+++ data_structure/queue.cpp
@@ -15,16 +15,41 @@ public:
     Queue() : length(0), queue{} {};
     ~Queue() {};
 
+    bool isEmpty() { return (length == 0); };
+    bool isFull() { return (length == size); };
+    T lookFirst() { return queue[0]; }
+
     void print();
+
+    T& operator[](uint index);
 };
 
 
 template<class T, uint size>
 void Queue<T, size>::print()
 {
-
+     if (isEmpty()){
+        std::cout << "QUEUE EMPTY";
+    }
+    else {
+        std::cout << "QUEUE: | ";
+        for (int i = 0; i < length; i++) {
+        std::cout << queue[i] << " | ";
+        }
+    }
+    std::cout << std::endl;
 }
 
+template<class T, uint size>
+T& Queue<T, size>::operator[](uint index) {
+    // Check index
+    if (index >= length){
+        std::cout << "Array index out of bound.";
+        exit(0);
+    }
+
+    return queue[index];
+}
 
 
 int main()
