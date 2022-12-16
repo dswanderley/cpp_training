8dba99f877fc14822fb8b3d4b775db05094ab0b9
Diego Wanderley <dswanderley@gmail.com>
2022-12-16 18:08:57 +0100

    Update producer_consumer.cpp
    Add condition variable to release mutex and wait for data in the array.

--- threads/producer_consumer.cpp
+++ threads/producer_consumer.cpp
@@ -5,57 +5,70 @@
 #include <condition_variable>
 #include <atomic>
 
+#define MAX_SIZE 1000000
+
 using namespace std::chrono_literals;
 
 
 std::atomic_bool runing{true};
 std::mutex g_mtx;
 std::vector<int> g_vector;
+std::condition_variable g_cond;
 
 
 void fnSource1()
 {
-    while (g_vector.size() < 1000000)
+    while (g_vector.size() < MAX_SIZE)
     {
-        //g_mtx.lock();
-        {
-            std::unique_lock<std::mutex> lock(g_mtx);
-            g_vector.push_back(1);
-        }
-        //g_mtx.unlock();
-        //std::this_thread::sleep_for(50ms);
+        // Lock mutex
+        std::unique_lock<std::mutex> lock(g_mtx);
+        // Update vector
+        g_vector.push_back(1);
+        // Unlock mutex
+        lock.unlock();
+        // Notify all threads
+        g_cond.notify_all();
+        // Wait
+        std::this_thread::sleep_for(2ms);
     }
 }
 
 
 void fnSource2()
 {
-    while (g_vector.size() < 1000000)
+    while (g_vector.size() < MAX_SIZE)
     {
+        // Lock mutex inside this scope
         {
             std::unique_lock<std::mutex> lock(g_mtx);
             g_vector.push_back(2);
         }
-        //std::this_thread::sleep_for(50ms);
+        // Notify all threads
+        g_cond.notify_all();
+        // Wait
+        std::this_thread::sleep_for(2ms);
     }
 }
 
 void fnReader()
 {
-    int i = 0;
     //std::this_thread::sleep_for(1s);
 
     while (runing)
     {
-        if (!g_vector.empty())
+        // Lock mutex inside this scope
         {
             std::unique_lock<std::mutex> lock(g_mtx);
-            i = g_vector.back();
-            g_vector.pop_back();
+            if (!g_vector.empty())
+            {
+                const int i = g_vector.back();
+                g_vector.pop_back();
 
-            std::cout << i << std::endl;
+                std::cout << i << std::endl;
+            }
+            // Wait for new elements
+            g_cond.wait(lock);
         }
-        //std::this_thread::sleep_for(10ms);
     }
 }
 
@@ -64,17 +77,19 @@ int main()
 {
     std::thread thread1, thread2, thread3;
 
+    thread3 = std::thread(&fnReader);
     thread1 = std::thread(&fnSource1);
     thread2 = std::thread(&fnSource2);
 
-    thread3 = std::thread(&fnReader);
 
-    std::this_thread::sleep_for(1s);
+    std::this_thread::sleep_for(2s);
     runing = false;
 
     thread3.join();
     thread1.detach();
     thread2.detach();
+
+    std::cout << "Vector size: " <<  g_vector.size() << std::endl;
 
     return 0;
 }
