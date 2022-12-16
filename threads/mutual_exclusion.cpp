641469d48aa3fc20fb51377302232fac322f654b
Diego Wanderley <dswanderley@gmail.com>
2022-12-16 17:34:55 +0100

    Update mutual_exclusion.cpp
    Modify to use only writers.

--- threads/mutual_exclusion.cpp
+++ threads/mutual_exclusion.cpp
@@ -3,19 +3,19 @@
 #include <thread>
 #include <chrono>
 #include <condition_variable>
-#include <atomic>
+
+#define MAX_SIZE 1000
 
 using namespace std::chrono_literals;
 
 
-std::atomic_bool runing{true};
 std::mutex g_mtx;
 std::vector<int> g_vector;
 
 
 void fnSource1()
 {
-    while (g_vector.size() < 1000000)
+    while (g_vector.size() < MAX_SIZE)
     {
         //g_mtx.lock();
         {
@@ -23,58 +23,50 @@ void fnSource1()
             g_vector.push_back(1);
         }
         //g_mtx.unlock();
-        //std::this_thread::sleep_for(50ms);
+        std::this_thread::sleep_for(1ms);
     }
 }
 
 
 void fnSource2()
 {
-    while (g_vector.size() < 1000000)
+    while (g_vector.size() < MAX_SIZE)
     {
         {
             std::unique_lock<std::mutex> lock(g_mtx);
             g_vector.push_back(2);
         }
-        //std::this_thread::sleep_for(50ms);
+        std::this_thread::sleep_for(1ms);
     }
 }
 
-void fnReader()
-{
-    int i = 0;
-    //std::this_thread::sleep_for(1s);
-
-    while (runing)
-    {
-        if (!g_vector.empty())
-        {
-            std::unique_lock<std::mutex> lock(g_mtx);
-            i = g_vector.back();
-            g_vector.pop_back();
-
-            std::cout << i << std::endl;
-        }
-        //std::this_thread::sleep_for(10ms);
-    }
-}
 
 
 int main()
 {
-    std::thread thread1, thread2, thread3;
+    std::thread thread1, thread2;
 
     thread1 = std::thread(&fnSource1);
     thread2 = std::thread(&fnSource2);
 
-    thread3 = std::thread(&fnReader);
+    thread1.join();
+    thread2.join();
 
-    std::this_thread::sleep_for(1s);
-    runing = false;
+    int c1, c2 = 0;
 
-    thread3.join();
-    thread1.detach();
-    thread2.detach();
+    for (int i = 0; i < g_vector.size(); i++)
+    {
+        if (g_vector[i] == 2)
+            c2++;
+        else if (g_vector[i] == 1)
+            c1++;
+        else
+            std::cout << "Error at index " << i << std::endl;
+    }
+
+    std::cout << "Vector size: " <<  g_vector.size() << std::endl;
+    std::cout << "Counter 1: " <<  c1 << std::endl;
+    std::cout << "Counter 2: " <<  c2 << std::endl;
 
     return 0;
 }
