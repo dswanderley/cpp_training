1706a4b6c33c91e43c6d6736524e08dcc32941d2
Diego Wanderley <dswanderley@gmail.com>
2022-12-19 18:46:55 +0100

    Update parallel_sum.cpp
    Add sequence of methods start, run and join threads.

--- threads/parallel_sum.cpp
+++ threads/parallel_sum.cpp
@@ -11,10 +11,6 @@ using namespace std::chrono;
 typedef unsigned int uint;
 
 
-
-void soma(){}
-
-
 class ParallelSum
 {
 
@@ -22,34 +18,21 @@ class ParallelSum
     ParallelSum(uint lenght, uint nT);
     ~ParallelSum();
     void sumFcn(uint start, uint end);
+    void join();
+    void run();
 
   private:
     std::vector<std::thread> vecThreads;
     std::vector<uint> vecData;
     uint nThreads;
+    std::atomic_bool waiting{true};
+
+    void startThreads();
 };
 
 ParallelSum::ParallelSum(uint length, uint nT) : vecData(length, 0), nThreads(nT)
 {
-    vecThreads = std::vector<std::thread>(nThreads);
-
-    uint interval = static_cast<uint>(length / nThreads);
-    uint remainder = static_cast<uint>(length % nThreads);
-
-    for (int t=0; t < nThreads; t++ )
-    {
-        uint start, end;
-
-
-
-        auto thd = std::thread(&ParallelSum::sumFcn, this, start, end);
-
-        vecThreads
-
-        thd.join();
-    }
-
-
+    startThreads();
 }
 
 ParallelSum::~ParallelSum()
@@ -58,8 +41,44 @@ ParallelSum::~ParallelSum()
 
 void ParallelSum::sumFcn(uint start, uint end)
 {
+    while (waiting)
+    {
+        /* code */
+    }
+}
+
+void ParallelSum::startThreads()
+{
+    const uint batch_len = static_cast<uint>(vecData.size() / nThreads);  // batch length
+    uint remainder = static_cast<uint>(vecData.size() % nThreads);  // remainder of the division
+    uint end=0, rem;                                     // Positions
+
+    for (int t=0; t < nThreads; t++ )
+    {
+        // Set extra (rem) value and decrease remainder positions
+        if (remainder > 0)  { remainder--; rem = 1; }
+        else { rem = 0; }
+        // Set star and end
+        const uint start = (t==0) ? 0 : end + 1;
+        end = start + (batch_len - 1) + rem;
+
+        vecThreads.push_back(std::thread(&ParallelSum::sumFcn, this, start, end));
+    }
 
 }
+
+void ParallelSum::join()
+{
+    for (auto& t: vecThreads) t.join();
+}
+
+void ParallelSum::run()
+{
+    // All threads are running but waiting.
+    waiting = false;
+}
+
+
 
 int main()
 {
@@ -67,6 +86,10 @@ int main()
     uint len = 2^12;
 
     ParallelSum pSum(len, 1);
+
+    pSum.run();
+
+    pSum.join();
 
 
     return 0;
