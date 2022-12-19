cd2c45ad5eaa7292f493c400997c2ce571564212
Diego Wanderley <dswanderley@gmail.com>
2022-12-19 20:27:14 +0100

    Update parallel_sum.cpp
    Add time calculation and thread finish flag.

--- threads/parallel_sum.cpp
+++ threads/parallel_sum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <chrono>
 #include <atomic>
@@ -17,22 +18,28 @@ class ParallelSum
   public:
     ParallelSum(uint lenght, uint nT);
     ~ParallelSum();
-    void sumFcn(uint start, uint end);
+    void sumFcn(uint start, uint end, uint thd_idx);
     void join();
     void run();
+    int64_t getSum() { return totalSum; }
+    bool getFinish() const;
 
   private:
-    std::vector<std::thread> vecThreads;
     std::vector<uint> vecData;
+    std::vector<bool> vecFinish;
+    std::vector<std::thread> vecThreads;
     uint nThreads;
-    std::atomic_bool waiting{true};
+    std::atomic<int64_t> totalSum;
+    std::atomic_bool waiting;
     std::mutex mtx;
     std::condition_variable cond;
 
     void startThreads();
 };
 
-ParallelSum::ParallelSum(uint length, uint nT) : vecData(length, 0), nThreads(nT)
+ParallelSum::ParallelSum(uint length, uint nT) :
+    vecData(length, 1), nThreads(nT),
+    vecFinish(nT, false), totalSum{0}, waiting{true}
 {
     startThreads();
 }
@@ -41,11 +48,10 @@ ParallelSum::~ParallelSum()
 {
 }
 
-void ParallelSum::sumFcn(uint start, uint end)
+void ParallelSum::sumFcn(uint start, uint end, uint thd_idx)
 {
     // Wait for waiting false
     {
-        // unique_lock prevents mutex from getting locked in case of exception
         std::unique_lock<std::mutex> lock(mtx);
         cond.wait(lock, [this]() -> bool
         {
@@ -53,6 +59,18 @@ void ParallelSum::sumFcn(uint start, uint end)
         });
     }
 
+    {
+        // Sum locally
+        int64_t localSum = 0;
+        for (uint i=start; i <= end; i++)
+        {
+            localSum += vecData[i];
+        }
+        // Update total
+        totalSum += localSum;
+
+        vecFinish[thd_idx] = true;
+    }
 }
 
 void ParallelSum::startThreads()
@@ -61,7 +79,7 @@ void ParallelSum::startThreads()
     uint remainder = static_cast<uint>(vecData.size() % nThreads);  // remainder of the division
     uint end=0, rem;                                     // Positions
 
-    for (int t=0; t < nThreads; t++ )
+    for (uint t=0; t < nThreads; t++ )
     {
         // Set extra (rem) value and decrease remainder positions
         if (remainder > 0)  { remainder--; rem = 1; }
@@ -70,7 +88,7 @@ void ParallelSum::startThreads()
         const uint start = (t==0) ? 0 : end + 1;
         end = start + (batch_len - 1) + rem;
 
-        vecThreads.push_back(std::thread(&ParallelSum::sumFcn, this, start, end));
+        vecThreads.push_back(std::thread(&ParallelSum::sumFcn, this, start, end, t));
     }
 }
 
@@ -92,18 +110,41 @@ void ParallelSum::run()
 }
 
 
+bool ParallelSum::getFinish() const
+{
+    bool res = std::all_of(vecFinish.begin(), vecFinish.end(), [](bool v) { return v; });
+
+    return res;
+}
 
 int main()
 {
 
-    uint len = 2^12;
+    uint len = 12;
 
-    ParallelSum pSum(len, 1);
+    auto t0 = high_resolution_clock::now();
+    ParallelSum pSum(len, 5);
 
+    auto t1 = high_resolution_clock::now();
     pSum.run();
 
-    pSum.join();
+    while (pSum.getFinish())
+    {
+        ;
+    }
+    auto t2 = high_resolution_clock::now();
 
+    pSum.join();
+    auto t3 = high_resolution_clock::now();
+
+
+    auto tTotal = duration_cast<milliseconds>(t3-t0);
+    auto tSum = duration_cast<milliseconds>(t1-t2);
+
+
+    std::cout << "Sum result: " << pSum.getSum() << "   |   ";
+    std::cout << "sum time: " << tSum.count() << " ms" << "   |   ";
+    std::cout << "total time: " << tTotal.count() << " ms";
 
     return 0;
 
