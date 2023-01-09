d147957cdbab20db1c6dfeb0feb5105ce085b365
Diego Wanderley <dswanderley@gmail.com>
2023-01-09 13:56:39 +0100

    Update double_linked_list.cpp
    Add insertion methods.

--- data_structure/double_linked_list.cpp
+++ data_structure/double_linked_list.cpp
@@ -21,7 +21,15 @@ class DoubleLinkedList
     ~DoubleLinkedList();
 
     void print() const;
+    void printBackward() const;
 
+    void push(const Data& data) { addNode(data, 0); };
+    void append(const Data& data) { addNode(data, length); };
+    void insertAt(const Data& data, uint idx) { addNode(data, idx); };
+
+  protected:
+    void addNode(const Data& data, uint index);
+    Data removeNode(uint index);
 
   private:
     std::shared_ptr<Node> head;
@@ -29,18 +37,19 @@ class DoubleLinkedList
     int length;
 
     static void advance(std::shared_ptr<Node>& node);
+    static void goBack(std::shared_ptr<Node>& node);
     static std::shared_ptr<Node> take(std::shared_ptr<Node>& node);
+
+    void startList(std::shared_ptr<Node>& newNode);
+    void insertAtInit(std::shared_ptr<Node>& newNode);
+    void insertAtEnd(std::shared_ptr<Node>& newNode);
+    void insertAtPos(std::shared_ptr<Node>& newNode, int index);
+    //void updateTail();
 };
 
 
-DoubleLinkedList::DoubleLinkedList(/* args */)
+DoubleLinkedList::DoubleLinkedList() : head(nullptr), tail(nullptr), length(0)
 {
-    head = std::make_shared<Node>();
-    tail = std::make_shared<Node>();
-    length = 0;
-
-    tail->prev = head;
-    head->next = tail;
 }
 
 DoubleLinkedList::~DoubleLinkedList()
@@ -67,6 +76,23 @@ void DoubleLinkedList::print() const
     std::cout << std::endl;
 }
 
+void DoubleLinkedList::printBackward() const
+{
+    std::shared_ptr<Node> temp = this->tail;
+
+    std::cout << "Printing list of length " << length << "." << std::endl;
+
+    int index = 0;
+    while( temp != nullptr ) {
+        std::cout << "(" << std::setw(2) << std::setfill('0') << index << ") ";
+        std::cout << "id: "             << temp->data.id << ", ";
+        std::cout << "description: "    << temp->data.description << std::endl;
+
+        goBack(temp);
+    }
+
+    std::cout << std::endl;
+}
 
 /* AUXILIAR METHODS */
 
@@ -78,6 +104,14 @@ void DoubleLinkedList::advance(std::shared_ptr<Node>& node)
     }
 }
 
+void DoubleLinkedList::goBack(std::shared_ptr<Node>& node)
+{
+    if (node != nullptr)
+    {
+        node = node->prev;
+    }
+}
+
 std::shared_ptr<Node> DoubleLinkedList::take(std::shared_ptr<Node>& node)
 {
     std::shared_ptr<Node> result = node;
@@ -85,13 +119,97 @@ std::shared_ptr<Node> DoubleLinkedList::take(std::shared_ptr<Node>& node)
     return result;
 }
 
+/* INSERTIONS METHODS */
+
+void DoubleLinkedList::addNode(const Data& data, uint index)
+{
+    if (index > length) {
+        std::cout << "Invalid position." << std::endl;
+        return;
+    }
+
+    std::shared_ptr<Node> newNode = std::make_shared<Node>();
+    newNode->data = data;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if (head == nullptr && tail == nullptr) {
+        startList(newNode);
+    }
+    else if (index == length) {
+        insertAtEnd(newNode);
+    }
+    else if (index == 0) {
+        insertAtInit(newNode);
+    }
+    else {
+       insertAtPos(newNode, index);
+    }
+
+    length++;
+}
+
+void DoubleLinkedList::startList(std::shared_ptr<Node>& newNode)
+{
+    head = newNode;
+    tail = newNode;
+}
+
+void DoubleLinkedList::insertAtInit(std::shared_ptr<Node>& newNode)
+{
+    // Update new data
+    newNode->next = head;
+    // Update head
+    head->prev = newNode;
+    head = newNode;
+}
+
+void DoubleLinkedList::insertAtEnd(std::shared_ptr<Node>& newNode)
+{
+    // Update new data
+    newNode->prev = tail;
+    // Update tail
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void DoubleLinkedList::insertAtPos(std::shared_ptr<Node>& newNode, int index)
+{
+    std::shared_ptr<Node> currNode = head;
+    int i = 0;
+    while (i < index-1)
+    {
+        advance(currNode);
+        i++;
+    }
+    // Update new node
+    newNode->next = currNode->next;
+    newNode->prev = currNode;
+    // Update current node
+    currNode->next = newNode;
+}
 
 
 int main()
 {
-    DoubleLinkedList dlList();
+    /* MY DATA */
+    Data data0{"Zero ground", 0};
+    Data data1{"First data", 1};
+    Data data2{"Second data", 2};
+    Data data3{"Third data", 3};
+    Data data4{"Something generic", 4};
 
+    /* DOUBLE LINKED LIST */
+    DoubleLinkedList dlList = DoubleLinkedList();
 
+    dlList.push(data2);
+    dlList.push(data1);
+    dlList.insertAt(data3, 2);
+    dlList.insertAt(data4, 3);
+    dlList.append(data0);
+
+    dlList.print();
+    dlList.printBackward();
 
     return 0;
 }
