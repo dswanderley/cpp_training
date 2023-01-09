610373a5c3cba8667fdab6dbf1661a703c0b4c9c
Diego Wanderley <dswanderley@gmail.com>
2023-01-09 19:39:05 +0100

    Update double_linked_list.cpp
    Add merge methods.

--- data_structure/double_linked_list.cpp
+++ data_structure/double_linked_list.cpp
@@ -31,6 +31,8 @@ class DoubleLinkedList
     Data remove() { return removeNode(0); };
     Data removeFrom(int idx) { return removeNode(idx); };
 
+    void sort() { mergeSort(head); updateTail(); };
+
   protected:
     void addNode(const Data& data, uint index);
     Data removeNode(int index);
@@ -44,7 +46,11 @@ class DoubleLinkedList
     static void goBack(std::shared_ptr<Node>& node);
     static std::shared_ptr<Node> take(std::shared_ptr<Node>& node);
 
-    //void updateTail();
+    static void mergeSort(std::shared_ptr<Node>& startNode);
+    static void split(std::shared_ptr<Node> source, std::shared_ptr<Node>& nodeFront, std::shared_ptr<Node>& nodeEnd);
+    static std::shared_ptr<Node> merge(std::shared_ptr<Node>& node1, std::shared_ptr<Node>& node2);
+
+    void updateTail();
     void startList(std::shared_ptr<Node>& newNode);
     void insertAtInit(std::shared_ptr<Node>& newNode);
     void insertAtEnd(std::shared_ptr<Node>& newNode);
@@ -87,7 +93,7 @@ void DoubleLinkedList::print() const
 
 void DoubleLinkedList::printBackward() const
 {
-    std::shared_ptr<Node> temp = this->tail;
+    std::shared_ptr<Node> temp = tail;
 
     std::cout << "Printing list of length " << length << "." << std::endl;
 
@@ -126,6 +132,19 @@ std::shared_ptr<Node> DoubleLinkedList::take(std::shared_ptr<Node>& node)
     std::shared_ptr<Node> result = node;
     advance(node);
     return result;
+}
+
+void DoubleLinkedList::updateTail()
+{
+    std::shared_ptr<Node> curr = head;
+    // Iterate the whole list
+    while (curr->next != nullptr)
+    {
+        advance(curr);
+    }
+    // Update tail
+    tail = curr;
+    tail->next = nullptr;
 }
 
 /* INSERTIONS METHODS */
@@ -292,6 +311,90 @@ Data DoubleLinkedList::removeFromPos(int idx)
     return data;
 }
 
+/* SORT METHODS */
+
+void DoubleLinkedList::split(std::shared_ptr<Node> source, std::shared_ptr<Node>& nodeFront, std::shared_ptr<Node>& nodeEnd)
+{
+    std::shared_ptr<Node> fast = source->next;
+    std::shared_ptr<Node> slow = source;
+    /* Iterate through the list to slow ptr achieve the middle. */
+    while (fast != nullptr)
+    {
+        advance(fast);
+        if (fast != nullptr)
+        {
+            advance(slow);      // Advance fast two nodes
+            advance(fast);      // and slow one node
+        }
+    }
+    // Update return
+    nodeFront = source;
+    nodeEnd = slow->next;
+    nodeEnd->prev = nullptr;
+    slow->next = nullptr;       // Stop first part in the middle
+}
+
+std::shared_ptr<Node> DoubleLinkedList::merge(std::shared_ptr<Node>& node1, std::shared_ptr<Node>& node2)
+{
+    if (node2 == nullptr)
+        return node1;
+
+    if (node1 == nullptr)
+        return node2;
+
+    std::shared_ptr<Node> mergedHead = (node2->data > node1->data) ? take(node1) : take(node2);
+    std::shared_ptr<Node> currNode = mergedHead;
+    std::shared_ptr<Node> prevNode = mergedHead;
+
+    while (node1 != nullptr && node2 != nullptr)
+    {
+        // Get smallest data and update current node
+        if (node1->data > node2->data)
+        {  // Update current node and node 2
+            currNode->next = take(node2);
+        }
+        else
+        {  // Update current node and node 1
+            currNode->next = take(node1);
+        }
+        // Update current node to next
+        advance(currNode);
+        currNode->prev = prevNode;
+        prevNode = currNode;
+    }
+
+    // In case of node 1 is not empty, complete the list with it
+    if (node1 != nullptr)
+    {
+        currNode->next = node1;
+        node1->prev = prevNode;
+    }
+    // In case of node 2 is not empty, complete the list with it
+    if (node2 != nullptr)
+    {
+        currNode->next = node2;
+        node2->prev = prevNode;
+    }
+
+    return mergedHead;
+}
+
+void DoubleLinkedList::mergeSort(std::shared_ptr<Node>& startNode)
+{
+    if (startNode == nullptr || startNode->next == nullptr)
+        return;
+
+    std::shared_ptr<Node> frontNode;
+    std::shared_ptr<Node> middleNode;
+    // Split list
+    split(startNode, frontNode, middleNode);
+    // Sort the splited lists recursively
+    mergeSort(frontNode);
+    mergeSort(middleNode);
+    // Merge two sorted lists
+    startNode = merge(frontNode, middleNode);
+}
+
 
 int main()
 {
@@ -305,17 +408,35 @@ int main()
     /* DOUBLE LINKED LIST */
     DoubleLinkedList dlList = DoubleLinkedList();
 
-    dlList.push(data2);
     dlList.push(data1);
-    dlList.insertAt(data3, 2);
-    dlList.insertAt(data4, 3);
-    dlList.append(data0);
+    //dlList.print();
+    //dlList.printBackward();
 
+
+    dlList.push(data2);
+    //dlList.print();
+    //dlList.printBackward();
+
+
+    dlList.insertAt(data4, 2);
+    //dlList.print();
+    //dlList.printBackward();
+
+
+    dlList.insertAt(data3, 3);
+    //dlList.print();
+    //dlList.printBackward();
+
+    dlList.append(data0);
+    dlList.print();
+    //dlList.printBackward();
+
+    /* Sort list by data */
+    std::cout << "SORT LIST" << std::endl << std::endl;
+
+    dlList.sort();
     dlList.print();
     dlList.printBackward();
-
-
-
 
     Data dataOut = dlList.pop();
     std::cout << "DATA REMOVED - id: " << dataOut.id << ", description: " << dataOut.description << std::endl << std::endl;
